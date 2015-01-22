b91ded47077e66486a462b5ebed0f2cb10c2f651
jake <bigjake2s@gmail.com>
2015-01-22 10:19:25 -0500

    More error checks for assets path

--- server/web/web.go
+++ server/web/web.go
@@ -1,19 +1,29 @@
 package web
 
 import (
-	"github.com/golang/glog"
 	"html/template"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/300brand/ocular8/server/config"
+	"github.com/golang/glog"
 )
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	appDir := filepath.Join(config.Config.WebAssets, "app")
+	assetsApp := filepath.Join(config.Config.WebAssets, "app")
+	appDir, err := filepath.Abs(assetsApp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		glog.Errorf("filepath.Abs(%s): %s", assetsApp, err)
+		return
+	}
 	appFiles := make([]string, 0, 16)
 	filepath.Walk(appDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			glog.Warningf("filepath.Walk(%s): %s", appDir, err)
+			return nil
+		}
 		if info.IsDir() {
 			return nil
 		}
@@ -33,11 +43,13 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		glog.Errorf("template.ParseFiles(%s): %s", indexPath, err)
+		return
 	}
 
 	data := struct{ AppFiles []string }{appFiles}
 	if err := t.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		glog.Errorf("template.Execute: %s", err)
+		return
 	}
 }
