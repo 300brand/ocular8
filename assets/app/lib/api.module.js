bc4a698e6bf7bd8081b2db57cb1cb95174c673b6
jake <bigjake2s@gmail.com>
2015-01-26 11:28:06 -0500

    Updated /api to /api/v1

--- assets/app/lib/api.module.js
+++ assets/app/lib/api.module.js
@@ -5,19 +5,19 @@ angular.module("api", [
 ])
 
 .factory("Pubs", ["$resource", function($resource) {
-	return $resource("/api/pubs/:pubid", null, {
+	return $resource("/api/v1/pubs/:pubid", null, {
 		"update": { method: "PUT" }
 	})
 }])
 
 .factory("PubFeeds", ["$resource", function($resource) {
-	return $resource("/api/pubs/:pubid/feeds/:feedid", null, {
+	return $resource("/api/v1/pubs/:pubid/feeds/:feedid", null, {
 		"update": { method: "PUT" }
 	})
 }])
 
 .factory("Feeds", ["$resource", function($resource) {
-	return $resource("/api/feeds/:feedid", null, {
+	return $resource("/api/v1/feeds/:feedid", null, {
 		"update": { method: "PUT" }
 	})
 }])
