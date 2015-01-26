95fa33fc3ac68ed26f0f163c44387f51ee5bee59
jake <bigjake2s@gmail.com>
2015-01-26 17:55:27 -0500

    Added interface to article portion of API

--- assets/app/lib/api.module.js
+++ assets/app/lib/api.module.js
@@ -11,13 +11,31 @@ angular.module("api", [
 }])
 
 .factory("PubFeeds", ["$resource", function($resource) {
-	return $resource("/api/v1/pubs/:pubid/feeds/:feedid", null, {
+	return $resource("/api/v1/pubs/:pubid/feeds/:feedid", { feedid: "@Id", pubid: "PubId" }, {
 		"update": { method: "PUT" }
 	})
 }])
 
 .factory("Feeds", ["$resource", function($resource) {
-	return $resource("/api/v1/feeds/:feedid", null, {
+	return $resource("/api/v1/feeds/:feedid", { feedid: "@Id" }, {
+		"update": { method: "PUT" }
+	})
+}])
+
+.factory("Articles", ["$resource", function($resource) {
+	return $resource("/api/v1/articles/:articleid", { articleid: "@Id" }, {
+		"update": { method: "PUT" }
+	})
+}])
+
+.factory("FeedArticles", ["$resource", function($resource) {
+	return $resource("/api/v1/feeds/:feedid/articles/:articleid", { articleid: "@Id", feedid: "@FeedId" }, {
+		"update": { method: "PUT" }
+	})
+}])
+
+.factory("PubArticles", ["$resource", function($resource) {
+	return $resource("/api/v1/pubs/:pubid/articles/:articleid", { articleid: "@Id", pubid: "@PubId" }, {
 		"update": { method: "PUT" }
 	})
 }])
