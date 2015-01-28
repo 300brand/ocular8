6b28238a2d82b9f6e3bdf4578462e6e0695a5186
jake <bigjake2s@gmail.com>
2015-01-28 14:26:09 -0500

    Download feed handler (in Python!)

--- handlers/download-feed/download-feed.py
+++ handlers/download-feed/download-feed.py
@@ -1,40 +1,82 @@
 #!/usr/bin/env python
 import argparse
 import bson
+import cleanurl
+import datetime
 import feedparser
 import pymongo
-import urlclean
+import time
+import urllib.error
 
 def process_feed(db, feed):
-	doc = feedparser.parse(feed['url'], etag=feed['etag'], modified=feed['lastmodified'])
+	etag = feed.get('etag', None)
+	modified = feed.get('lastmodified', None)
+	doc = feedparser.parse(feed['url'], etag=etag, modified=modified)
+
+	print("STATUS", doc['status'])
 
 	update = {
 		'bozo':         doc['bozo'],
-		'etag':         doc['etag'],
-		'lastmodified': doc['modified'],
+		'etag':         doc.get('etag', None),
+		'lastmodified': doc.get('modified', None),
 		'laststatus':   doc['status'],
-		'updated':      doc['updated_parsed']
+		'lastdownload': datetime.datetime.utcnow(),
+		'nextdownload': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
 	}
+	if 'updated' in doc:
+		t = datetime.datetime.utcfromtimestamp(time.mktime(doc['updated_parsed']))
+		update['updated'] = t
+	if doc['status'] == 301:
+		update['originalurl'] = feed['url']
+		update['url'] = doc['href']
+
 	db.feeds.update({ '_id': feed['_id'] }, { '$set': update })
 
-	for entry in feed.entries:
+	if doc['status'] == 410 or doc['status'] == 304:
+		print(doc)
+		return
+
+	for entry in doc.entries:
+		try:
+			url, req = cleanurl.clean(entry['link'])
+		except urllib.error.HTTPError as err:
+			print("ERROR", err, entry['link'])
+			db.article_errors.insert({
+				'url':    entry['link'],
+				'code':   err.code,
+				'reason': err.reason
+			})
+			continue
+
+		if db.articles.find_one({ 'url': url }) is not None:
+			continue
+
+		published = entry.get('published_parsed', None)
+		if published is not None:
+			published = datetime.datetime.utcfromtimestamp(time.mktime(published))
+
+		html = req.read()
+
 		article = {
 			'_id':       bson.objectid.ObjectId(),
 			'feedid':    feed['_id'],
 			'pubid':     feed['pubid'],
-			'author':    entry['author'],
-			'published': entry['published_parsed'],
-			'title':     entry['title'],
-			'url':       urlclean.unshorten(entry['link']),
+			'author':    entry.get('author', None),
+			'published': published,
+			'title':     entry.get('title', None),
+			'url':       url,
+			'html':      html,
 			'entry':     {
-				'author':    entry['author'],
-				'published': entry['published_parsed'],
+				'author':    entry.get('author', None),
+				'published': published,
+				'title':     entry.get('title', None),
 				'url':       entry['link']
 			}
 		}
-		article_exists = (db.articles.find_one({ 'url': article['url'] }) == None)
-		if not article_exists:
-			db.articles.insert(article)
+		
+		print("ARTICLE", article['_id'])
+		db.articles.insert(article)
+
 
 def main():
 	parser = argparse.ArgumentParser(description='Download feed and push new URLs into next queue')
@@ -48,6 +90,7 @@ def main():
 	client = pymongo.MongoClient(args.mongo)
 
 	db = client.get_default_database()
+	db.articles.ensure_index('url', unique=True)
 
 	bson_ids = [ bson.objectid.ObjectId(id) for id in args.id ]
 	feeds = db.feeds.find({ '_id': { '$in': bson_ids } })
