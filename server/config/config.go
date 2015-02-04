0b50689979a35c5b4892260696d8746a6a3f1d1a
jake <bigjake2s@gmail.com>
2015-02-04 09:12:42 -0500

    Added NSQ Lookupd config options

--- server/config/config.go
+++ server/config/config.go
@@ -5,23 +5,25 @@ import (
 )
 
 type ConfigType struct {
-	Etcd      string
-	Mongo     string
-	NsqdHTTP  string
-	NsqdTCP   string
-	Handlers  string
-	WebAssets string
-	WebListen string
+	Etcd          string
+	Mongo         string
+	NsqdHTTP      string
+	NsqdTCP       string
+	NsqLookupdTCP string
+	Handlers      string
+	WebAssets     string
+	WebListen     string
 }
 
 var Config = &ConfigType{
-	"http://127.0.0.1:4001",
-	"mongodb://127.0.0.1:27017/ocular8",
-	"http://127.0.0.1:4151",
-	"127.0.0.1:4150",
-	".",
-	".",
-	":8080",
+	Etcd:          "http://127.0.0.1:4001",
+	Mongo:         "mongodb://127.0.0.1:27017/ocular8",
+	NsqdHTTP:      "http://127.0.0.1:4161",
+	NsqdTCP:       "127.0.0.1:4150",
+	NsqLookupdTCP: "127.0.0.1:4160",
+	Handlers:      ".",
+	WebAssets:     ".",
+	WebListen:     ":8080",
 }
 
 func init() {
@@ -31,6 +33,7 @@ func init() {
 	flag.StringVar(&Config.Etcd, "etcd", Config.Etcd, "Etcd server for configs")
 	flag.StringVar(&Config.NsqdHTTP, "nsqdhttp", Config.NsqdHTTP, "Nsqd server for queue (HTTP)")
 	flag.StringVar(&Config.NsqdTCP, "nsqdtcp", Config.NsqdTCP, "Nsqd server for queue (TCP)")
+	flag.StringVar(&Config.NsqLookupdTCP, "nsqlookupdtcp", Config.NsqLookupdTCP, "Nsq lookupd server for queue (TCP)")
 	flag.StringVar(&Config.Mongo, "mongo", Config.Mongo, "Mongo server")
 }
 
