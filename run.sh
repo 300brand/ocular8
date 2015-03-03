dbf805a8378dff0bb5f377c51d067e43eb090f24
jake <bigjake2s@gmail.com>
2015-03-03 13:33:08 -0500

    Updated build logic from @zeebo

--- run.sh
+++ run.sh
@@ -3,21 +3,25 @@ set -o errexit -o nounset
 
 CMD=${1:-}
 ROOT=$(dirname $0)
-GOHANDLERS="enqueue-feeds download-entry recount-articles"
 
 function build {
 	go get -v github.com/300brand/ocular8/server
 	cp -a $GOPATH/bin/server $ROOT/o8-server
-	for H in $GOHANDLERS; do
-		go get -v github.com/300brand/ocular8/handlers/$H
-		cp -a $GOPATH/bin/$H $ROOT/handlers/$H/
+	HANDLERS=$(go list -f '{{.ImportPath}}^{{.Target}}' github.com/300brand/ocular8/handlers/...)
+	for HANDLER in $HANDLERS; do
+		IMPORTPATH=$(echo $HANDLER | cut -d^ -f1)
+		TARGET=$(echo $HANDLER | cut -d^ -f2)
+		go get -v $IMPORTPATH
+		cp -a $TARGET $GOPATH/src/$IMPORTPATH/
 	done
 }
 
 function clean {
-	rm $ROOT/o8-server
-	for H in $GOHANDLERS; do
-		rm $ROOT/handlers/$H/$H
+	rm -vf $ROOT/o8-server
+	DIRS=$(go list -f '{{.Dir}}' github.com/300brand/ocular8/handlers/...)
+	for DIR in $DIRS; do
+		BIN=$(basename $DIR)
+		rm -vf $DIR/$BIN
 	done
 }
 
@@ -30,7 +34,24 @@ function pydeps {
 		python3 -m pip install --user --requirement $R
 	done
 
-	(cd /tmp && git clone https://github.com/grangier/python-goose.git && cd python-goose && python2 setup.py install --user)
+	( \
+		cd /tmp && \
+		git clone https://github.com/grangier/python-goose.git && \
+		cd python-goose && \
+		python2 setup.py install --user && \
+		cd /tmp && \
+		rm -rf /tmp/python-goose \
+	)
+
+	( \
+		cd /tmp && \
+		git clone https://github.com/jplana/python-etcd.git && \
+		cd python-etcd && \
+		python2 setup.py install --user && \
+		python3 setup.py install --user && \
+		cd /tmp && \
+		rm -rf /tmp/python-etcd \
+	)
 }
 
 function start {
