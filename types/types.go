6d8abcffa938c9bde81152908b66d062f00183be
jake <bigjake2s@gmail.com>
2015-03-03 08:27:21 -0500

    Removing LexisNexis components from structs

--- types/types.go
+++ types/types.go
@@ -7,49 +7,43 @@ import (
 )
 
 type Pub struct {
-	Id           bson.ObjectId `bson:"_id"`
-	IsLexisNexis bool
-	Name         string
-	Homepage     string
-	Description  string
-	NumArticles  int
-	NumFeeds     int
-	NumReaders   int
-	XPathBody    []string
-	XPathAuthor  []string
-	XPathDate    []string
-	XPathTitle   []string
-	LastUpdate   time.Time
-	LexisNexis   *LexisNexisPub
+	Id          bson.ObjectId `bson:"_id"`
+	Name        string
+	Homepage    string
+	Description string
+	NumArticles int
+	NumFeeds    int
+	NumReaders  int
+	XPathBody   []string
+	XPathAuthor []string
+	XPathDate   []string
+	XPathTitle  []string
+	LastUpdate  time.Time
 }
 
 type Feed struct {
 	Id           bson.ObjectId `bson:"_id"`
 	PubId        bson.ObjectId
-	IsLexisNexis bool
 	Url          string
 	NumArticles  int
 	LastDownload time.Time
 	NextDownload time.Time
-	LexisNexis   *LexisNexisFeed
 }
 
 type Article struct {
-	Id           bson.ObjectId `bson:"_id"`
-	FeedId       bson.ObjectId
-	PubId        bson.ObjectId
-	IsLexisNexis bool
-	Url          string
-	Title        string
-	Author       string
-	Published    time.Time
-	BodyText     string
-	BodyHTML     string
-	HTML         []byte
-	LoadTime     time.Duration
-	Entry        *EntryInfo
-	Goose        *GooseInfo
-	LexisNexis   *LexisNexisArticle
+	Id        bson.ObjectId `bson:"_id"`
+	FeedId    bson.ObjectId
+	PubId     bson.ObjectId
+	Url       string
+	Title     string
+	Author    string
+	Published time.Time
+	BodyText  string
+	BodyHTML  string
+	HTML      []byte
+	LoadTime  time.Duration
+	Entry     *EntryInfo
+	Goose     *GooseInfo
 }
 
 type EntryInfo struct {
